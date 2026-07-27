9173910af508112323af0e0f2003c646e77f12b2
javier <gamingjavi21@gmail.com>
2026-07-27 09:28:36 +0700

    Navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MaterialApp(title: "Javierere", home: new Hallow()));
+  runApp(
+    new MaterialApp(
+      title: "Haviereere",
+      home: new Nyoba(),
+      routes: <String, WidgetBuilder>{
+        '/halsatu': (BuildContext context) => new Nyoba(),
+        '/haldua': (BuildContext context) => new Nyobs(),
+      },
+    ),
+  );
 }
 
-class Hallow extends StatelessWidget {
+class Nyoba extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        leading: new Icon(Icons.android_outlined),
-        title: new Center(
-          child: new Text(
-            "Javierere",
-            style: new TextStyle(color: Colors.white, fontFamily: "Serif"),
-          ),
-        ),
-        actions: <Widget>[new Icon(Icons.search)],
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.android),
+        title: new Text("Javaieerre"),
       ),
       body: new Container(
         child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            new Center(
-              child: new Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  new Icon(Icons.local_pizza),
-                  new Icon(Icons.done_all),
-                  new Icon(Icons.done_sharp),
-                ],
-              ),
+            MyCard(
+              icon: Icons.home,
+              color: Colors.black,
+              text: "Home",
+              routename: "/haldua",
             ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
-            new Icon(Icons.local_pizza),
-            new Icon(Icons.done_all),
-            new Icon(Icons.done_sharp),
+class Nyobs extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.blue,
+        leading: Icon(Icons.android),
+        title: new Text("Javaieerre"),
+      ),
+      body: new Container(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            MyCard(
+              icon: Icons.tap_and_play,
+              color: Colors.greenAccent,
+              text: "Home",
+              routename: "/halsatu",
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MyCard extends StatelessWidget {
+  MyCard({
+    required this.icon,
+    required this.color,
+    required this.text,
+    required this.routename,
+  });
+
+  final IconData icon;
+  final Color color;
+  final String text;
+  final String routename;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: EdgeInsets.all(10.0),
+      child: new Card(
+        child: new Column(
+          children: <Widget>[
+            new IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, routename);
+              },
+              icon: new Icon(icon, size: 50.0, color: color),
+            ),
+            new Text(text),
           ],
         ),
       ),
