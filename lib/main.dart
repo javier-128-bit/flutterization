43c9ad29bfd3d07dfa772a6f20a2725a3f9d6c94
javier <gamingjavi21@gmail.com>
2026-07-29 22:58:50 +0700

    input text,dialog,snackbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(new MaterialApp(home: new Home()));
+  runApp(new MaterialApp(title: "Javierere", home: new Home()));
 }
 
 class Home extends StatefulWidget {
@@ -13,38 +10,86 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List dataJson = [];
+  final TextEditingController controllerInput = new TextEditingController();
+  final TextEditingController controllerAlert = new TextEditingController();
+  final TextEditingController controllerSnackBar = new TextEditingController();
 
-  Future<void> ambilData() async {
-    http.Response hasil = await http.get(
-      Uri.parse("https://jsonplaceholder.typicode.com/posts"),
-      headers: {"Accept": "application/json"},
+  String teks = "";
+
+  void _snackBar(BuildContext context, String str) {
+    if (str.isEmpty) return;
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(str), duration: new Duration(seconds: 3)),
     );
-
-    this.setState(() {
-      dataJson = json.decode(hasil.body);
-    });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    ambilData();
+  void _alertDialog(str) {
+    if (str.isEmpty) return;
+    AlertDialog alertDialog = new AlertDialog(
+      content: new Text(
+        str,
+        style: new TextStyle(color: Colors.black, fontSize: 20.0),
+      ),
+      actions: <Widget>[
+        new ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: new Text("Ok"),
+        ),
+      ],
+    );
+    showAboutDialog(context: context, children: [alertDialog]);
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(title: new Text("Javiererere")),
+      appBar: new AppBar(
+        title: new Text("This Is Javier"),
+        backgroundColor: Colors.purple,
+      ),
+
       body: new Container(
-        child: new ListView.builder(
-          itemCount: (dataJson == null ? 0 : dataJson.length),
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: new Text(dataJson[index]["title"]),
-              subtitle: new Text(dataJson[index]["body"]),
-            );
-          },
+        child: new Column(
+          children: [
+            new TextField(
+              controller: controllerInput,
+              decoration: new InputDecoration(hintText: "Tulis Disini Dongs"),
+              onSubmitted: (String str) {
+                setState(() {
+                  teks = str + "\n" + teks;
+                  controllerInput.text = "";
+                });
+              },
+            ),
+            new Text(
+              teks,
+              style: TextStyle(fontSize: 20.0, color: Colors.black),
+            ),
+
+            new TextField(
+              controller: controllerAlert,
+              decoration: new InputDecoration(hintText: "Tulis Disini Dongs"),
+              onSubmitted: (String str) {
+                setState(() {
+                  _alertDialog(str);
+                  controllerAlert.text = "";
+                });
+              },
+            ),
+
+            new TextField(
+              controller: controllerSnackBar,
+              decoration: new InputDecoration(hintText: "Tulis Disini Dongs"),
+              onSubmitted: (String str) {
+                setState(() {
+                  _snackBar(context, str);
+                });
+              },
+            ),
+          ],
         ),
       ),
     );
