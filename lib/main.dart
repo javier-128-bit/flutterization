bb630ef6a65c2a547db9a6e69eb7d9f457b44354
javier <gamingjavi21@gmail.com>
2026-07-31 23:51:49 +0700

    Hero Section, gridview,ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,157 @@
 import 'package:flutter/material.dart';
-import 'detail.dart';
 
 void main() {
-  runApp(new MaterialApp(title: "Konz", home: new Home()));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatefulWidget {
   @override
-  _Homestate createState() => _Homestate();
+  _HomeState createState() => _HomeState();
 }
 
-class _Homestate extends State<Home> {
-  String gambar1 = "img/komputer.jpg";
-  String gambar2 = "img/smarphone.jpg";
-  String backup = "";
+class _HomeState extends State<Home> {
+  List<Container> daftarSuperhero = [];
 
-  String nama1 = "Javier";
-  String nama2 = "Ilham";
-  String backupnama = "";
+  var karakter = [
+    {"nama": "Komputer", "gambar": "img/komputer.jpg"},
+    {"nama": "Smartphone", "gambar": "img/smarphone.jpg"},
+  ];
 
-  void gantiUser() {
-    setState(() {
-      backup = gambar1;
-      gambar1 = gambar2;
-      gambar2 = backup;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _buatlist();
   }
 
-  void gantiNama() {
-    setState(() {
-      backup = nama1;
-      nama1 = nama2;
-      nama2 = backup;
-    });
+  void _buatlist() {
+    for (var item in karakter) {
+      daftarSuperhero.add(
+        Container(
+          alignment: Alignment.center,
+          child: Card(
+            child: Column(
+              children: [
+                Hero(
+                  tag: item["nama"] ?? "",
+                  child: InkWell(
+                    child: Image.asset(item["gambar"] ?? "", fit: BoxFit.cover),
+                    onTap: () => Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (BuildContext context) => Detail(
+                          nama: item["nama"] ?? "",
+                          gambar: item["gambar"] ?? "",
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(10.0)),
+                Text(item["nama"] ?? ""),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Nyoba drawer katanya canggih nih"),
-        backgroundColor: Colors.red,
-      ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: [
-            new UserAccountsDrawerHeader(
-              currentAccountPicture: new GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pop();
-                  Navigator.of(context).push(
-                    new MaterialPageRoute(
-                      builder: (BuildContext context) =>
-                          new Detail(nama: nama1, image: gambar1),
+    return Scaffold(
+      appBar: AppBar(title: Text("New Hero Section")),
+      body: GridView.count(crossAxisCount: 2, children: daftarSuperhero),
+    );
+  }
+}
+
+class Detail extends StatelessWidget {
+  Detail({required this.gambar, required this.nama});
+  final String gambar;
+  final String nama;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Hero(tag: this.nama, child: Image.asset(this.gambar)),
+          Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      this.nama,
+                      style: TextStyle(fontSize: 20.0, color: Colors.blue),
                     ),
-                  );
-                },
-                child: new CircleAvatar(backgroundImage: AssetImage(gambar1)),
-              ),
-              accountName: new Text(nama1),
-              accountEmail: new Text("Javier@gmail.com"),
-              decoration: new BoxDecoration(color: Colors.red),
-              otherAccountsPictures: [
-                GestureDetector(
-                  onTap: () {
-                    gantiUser();
-                    gantiNama();
-                  },
-                  child: new CircleAvatar(backgroundImage: AssetImage(gambar2)),
+                    Text(
+                      "${this.nama}@gmail.com",
+                      style: TextStyle(fontSize: 20.0, color: Colors.blue),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-            new ListTile(
-              title: new Text("Setting"),
-              trailing: new Icon(Icons.settings),
-            ),
-            new ListTile(
-              title: new Text("Close"),
-              trailing: new Icon(Icons.close),
-            ),
-          ],
-        ),
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.all(10.0)),
+                  Text("12"),
+                  Icon(Icons.star),
+                ],
+              ),
+            ],
+          ),
+
+          Row(
+            children: [
+              Padding(padding: EdgeInsets.all(10.0)),
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(Icons.call),
+                    Text(
+                      "Call Me if you can",
+                      style: TextStyle(fontSize: 10.0, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(Icons.call),
+                    Text(
+                      "Call Me if you can",
+                      style: TextStyle(fontSize: 10.0, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Icon(Icons.call),
+                    Text(
+                      "Call Me if you can",
+                      style: TextStyle(fontSize: 10.0, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.all(10.0)),
+              Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 20.0, color: Colors.black),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: new Container(),
     );
   }
 }
